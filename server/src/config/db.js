7efa03e4ea5bef71a2c446a7c18986d48bdc6bc3
Oghenemairo James <oghenemairo2020@gmail.com>
2026-09-09 15:14:55 +0100

    Configure database SSL certificates

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 const path = require("path");
+const fs = require("fs");
 
 dotenv.config({
   path: path.join(__dirname, "../../.env"),
 });
+
+const caPath = path.join(__dirname, "../../certs/ca.pem");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -12,6 +15,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    ca: fs.readFileSync(caPath),
+    rejectUnauthorized: true,
+  },
 });
 
 module.exports = pool;
